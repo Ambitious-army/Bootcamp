b915b6d178e7ad079d55aa05e96bb9f8a5301ce6
Ambitious-army <97444606+Ambitious-army@users.noreply.github.com>
2022-01-19 20:53:19 +0530

    Add files via upload

---
+++ C/Assignments/Yuvraj Singh/c2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+
+    int a;
+    int b;
+    printf("Enter your first number\n");
+    scanf("%d", &a);
+    printf("Enter your second number\n");
+    scanf("%d", &b);
+    printf("The sum is %d", a + b);
+}
