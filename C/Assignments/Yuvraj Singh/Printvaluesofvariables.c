8da5d6966bb3393c83a03cd0bfac6df73000d854
Ambitious-army <97444606+Ambitious-army@users.noreply.github.com>
2022-01-20 21:29:54 +0530

    Add files via upload

---
+++ C/Assignments/Yuvraj Singh/Printvaluesofvariables.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+   int a = 65 , b = 45;
+   float floatingnumber = 56.78;
+   char character = 'A';
+
+   printf("a = %d , b = %d , floatingnumber = %.5f , character = %c" , b , a , floatingnumber , character );
+}
---
+++ C/Assignments/Yuvraj Singh/Storevalueofvariable.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    printf("Enter an integer : ");
+    scanf("%d" , &a);
+    float f;
+    printf("Enter a float value : ");
+    scanf("%f" , &f);
+    char c;
+    printf("Enter a character : ");
+    scanf("%c" , &c);
+}
