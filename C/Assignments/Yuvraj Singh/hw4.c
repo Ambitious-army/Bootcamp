815787a864c24dbaae49fed82c8b68c8d6a93115
Yuvrajsinggh <90794533+Yuvrajsinggh@users.noreply.github.com>
2022-01-25 16:40:02 +0530

    Add files via upload

---
+++ C/Assignments/Yuvraj Singh/hw4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+// Yuvraj Singh
+
+int main () {
+    printf("Enter the Alphabet :\n");
+    char ch ;
+    scanf("%c", &ch );
+
+    switch(ch) {
+        case 'A':
+            printf("APPLE\n");
+            break;
+
+        case 'B' :
+            printf("BALL\n");
+            break;
+
+        case 'C' :
+            printf("CALL\n");
+            break;
+
+        case 'D' :
+            printf("DOG\n");
+            break;
+
+        default:
+            printf("Wrong! \n");
+            break;
+    }
+
+}
